383f77887aefd0dfd8e3ebd3f0cd7b541ece4adf
Ezequiel <ezegonzalez800@gmail.com>
2024-05-28 23:35:41 -0300

    sin variables.scc

--- pdfJuridico/capacitor.config.ts
+++ pdfJuridico/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'integracion',
+  appName: 'Admin legal',
   webDir: 'www',
   server: {
     androidScheme: 'https'
